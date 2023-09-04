5ff5ff552014c45bed3b5e689f22e0cebcd92340
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-09-04 23:12:28 +0530

    Updated

--- Backend/Controllers/Token.js
+++ Backend/Controllers/Token.js
@@ -2,16 +2,18 @@ import random from "random";
 import TokenModal from "../Modals/TokenModal.js";
 
 function generateToken(){
-return random.int(10000,20000)
+return random.int(100000,999999)
 }
 
-setInterval(()=>{
-    generateToken()
-    },5000)
 
 export const createToken=async(req,res)=>{
+
     try{
-    const token=new TokenModal({token:generateToken()})
+    const token=new TokenModal({token:
+        setInterval(()=>{
+            generateToken()
+            },5000)})
+    console.log(token,"Genreted")
     await token.save()
     return res.status(200).json({success:"true",token:token})
     }
